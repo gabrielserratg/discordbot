bf59e8d2b77f7a55177bb721e6b03ded6b0b3295
Gabriel Serra <49370091+gabrielserratg@users.noreply.github.com>
2020-04-18 20:36:35 -0300

    Add files via upload

---
+++ users.py
@@ -0,0 +1,7 @@
+users = {
+    'Tatumkhamun#2312': ['Tatumkhamun'],
+    'CSL619#8493': ['CSL619'],
+    'FluffBunker#1757': ['FluffBunker'],
+    'Arcanice#1327': ['Sharkie668'],
+    'Ozza360#4826': ['Ozza360']
+}
